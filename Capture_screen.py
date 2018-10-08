1e61f10d2df744349b0a1e5d396a502b465f2158
Roel <tijerinaroel@gmail.com>
2018-10-07 23:18:37 -0500

    Capture Screen set up
    -Set up to take screenshots of monitor every x minutes

--- Capture_screen.py
+++ Capture_screen.py
@@ -1,16 +1,40 @@
+import cv2
 
 import mss
 import mss.tools
+import schedule
+import time
 
-#15 seconds
-with mss.mss() as sct:
-    # The screen part to capture
-    monitor = {"top": 300, "left": 65, "width": 800, "height": 710}
-    output = "images/f.png".format(**monitor)
+import time
 
-    # Grab the data
-    sct_img = sct.grab(monitor)
+#VARIABLES
 
-    # Save to the picture file
-    mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-    print(output)
+#image name is timestamp
+current_time = ""
+elapse_time = 5
+
+
+def job():
+    current_time = time.strftime("%H%M")
+    with mss.mss() as sct:
+        # The screen part to capture
+        monitor = {"top": 300, "left": 65, "width": 825, "height": 710}
+        output = "images/"+current_time+".png".format(**monitor)
+
+        # Grab the data
+        sct_img = sct.grab(monitor)
+
+        # Save to the picture file
+        mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+        print(output)
+
+schedule.every(elapse_time).minutes.do(job)
+
+#CTRL + F2 to stop it
+try:
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+
--- Data_analysis.py
+++ Data_analysis.py
@@ -1,34 +1,60 @@
+import numpy as np
 import pytesseract
+import cv2
+from PIL import Image
+import threading
+import re
+
+Pri = []
+Duo = []
+Tri = []
+Tet = []
+Pen = []
+
+failedPri = []
+failedDuo = []
+failedTri = []
+failedTet = []
+failedPen = []
+
+
+regex = '(got|failed at)\s?.+?(PRI|DUO|TRI|TET|PEN):\s{1}(s?.+?)\s?(?:ll|Enhancement Failed|Enhancement Success)\s?.+?(\d+:\d+)'
 
 
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 
-from PIL import Image
-import threading
 
 text =""
 text2=""
 def f():
-    text = pytesseract.image_to_string(Image.open('images/b.png'))
-    print "text: ",text
-def f2():
-    text2 = pytesseract.image_to_string(Image.open('images/c.png'))
-    print "text2: ", text2
 
-t1 = threading.Thread(target=f)
+    text = pytesseract.image_to_string(Image.open('images/ff.png'))
+    print text
+def f2():
+    text2 = pytesseract.image_to_string(Image.open('images/ff.png'))
+    print  text2
+
+#t1 = threading.Thread(target=f)
 t2 = threading.Thread(target=f2)
 
-t1.start()
+#t1.start()
 # starting thread 2
 t2.start()
 
 # wait until thread 1 is completely executed
-t1.join()
+#t1.join()
 # wait until thread' 2 is completely executed
 t2.join()
 
 array1 = []
 array2=[]
 
+def parse(text):
+    print"hi"
+
+
+
+
 print("Done!")
+
